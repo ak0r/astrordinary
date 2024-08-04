f026de085ebfa6be67e607d1566da82a17698e3f
Amit K <androidak0r@gmail.com>
2024-08-04 23:12:53 +0530

    refactor: Working on boilerplate
    WIP - Boilerplate Version

---
+++ src/configs/consts.ts
@@ -0,0 +1,6 @@
+export const SiteMeta = {
+  TITLE: "Astrordinary",
+  DESC: "Starter template for Astro.build with Tailwind CSS",
+  URL: "htps://example.com",
+  EMAIl: "androidak0r@gmail.com"
+};
