95f219b5049c81442696d3372493f6c0d0d432b3
Amit K <androidak0r@gmail.com>
2024-08-04 19:27:24 +0530

    build: Updated configs
    Updated configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+const isProd = import.meta.env.PROD;
 
 // https://astro.build/config
 export default defineConfig({
+  site: isProd ? 'https://amitkul.in' : 'http://localhost:4321',
   integrations: [tailwind()]
 });
