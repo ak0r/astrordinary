2a19c49411285d8254156b1452a03291b4b7c00a
Amit K <androidak0r@gmail.com>
2024-08-05 15:33:58 +0530

    feat: Added custom link component
    Added custom link component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import icon from "astro-icon";
 const isProd = import.meta.env.PROD;
+
 
 // https://astro.build/config
 export default defineConfig({
   site: isProd ? 'https://example.com' : 'http://localhost:4321',
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
--- src/configs/consts.ts
+++ src/configs/consts.ts
@@ -1,3 +1,4 @@
+// The base details of site
 export const SiteMeta = {
   siteTitle: "Astrordinary",
   siteDesc: "Starter template for Astro.build with Tailwind CSS",
@@ -8,3 +9,25 @@ export const SiteMeta = {
     'Travel Blogger',
   ],
 };
+
+// Navbar menu links
+export const headerLinks = [
+  { title: "Articles", url: "/articles", },
+  { title: "Galleries", url: "/galleries", },
+  { title: "About", url: "/about", },
+];
+
+// footer menu links
+export const footerLinks = [
+  { title: "Home", url: "/", },
+  { title: "About", url: "/about", },
+];
+
+// Social menu links
+export const socialLinks = [
+  { name: "facebook", link: "https://www.facebook.com/", icon: "fa6-brands:facebook-f" },
+  { name: "twitter", link: "https://www.facebook.com/", icon: "fa6-brands:x-twitter" },
+  { name: "instagram", link: "https://www.instagram.com/", icon: "fa6-brands:instagram" },
+  { name: "youtube", link: "https://www.youtube.com/", icon: "fa6-brands:youtube" },
+  { name: "github", link: "https://www.github.com/", icon: "fa6-brands:github" },
+];
